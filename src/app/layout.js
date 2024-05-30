a5ee676048b21f52224a53a3c6890a52ca88a3a1
mechaneyes <ray@mechaneyes.com>
2024-05-29 23:08:46 -0400

    lotta metadata editing ° og:images ° favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,40 @@
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const favicon = {
+  icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👁️</text></svg>",
+};
+
 export const metadata = {
   title: "Ray Weitzenberg",
   description: "Try to find me.",
+  url: "https://nysee.nyc/",
+  type: "website",
+  images: "https://nysee.nyc/images/noOrdinaryMonkey.jpg",
+  openGraph: {
+    images: "https://nysee.nyc/images/og-image.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@mechaneyes",
+    title: "Ray Weitzenberg",
+    description: "Try to find me.",
+    image: "https://nysee.nyc/images/twitter-card.png",
+  },
+  robots: "index, follow",
+  keywords:
+    "Ray Weitzenberg, photographer, artist, technologist, new yorker, NYSEE, NYC",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href={favicon.icon} />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );

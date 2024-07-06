5f436c7b5817a438206b4353763a14b3db8fd779
mechaneyes <ray@mechaneyes.com>
2024-07-05 20:20:57 -0400

    scopus ex presentation pdf ° favicon
    ° added page to present scopus ex machina presentation given last monday in class
    ° leveraged react-pdf-viewer to load the pdf
    ° tweaked icon.js to use a whirligig character for favicon

--- src/app/(pages)/scopus-ex-presentation/layout.js
+++ src/app/(pages)/scopus-ex-presentation/layout.js
@@ -6,20 +6,20 @@ import "@/app/styles/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "NYSee",
-  description: "Try to find me.",
+  title: "Present Ex Machina",
+  description: "Presenting Scopus Ex Machina",
   url: "https://nysee.nyc/",
   type: "website",
-  images: "https://nysee.nyc/images/noOrdinaryMonkey.jpg",
+  images: "https://nysee.nyc/images/og-presentation.png",
   openGraph: {
-    images: "https://nysee.nyc/images/og-image.png",
+    images: "https://nysee.nyc/images/og-presentation.png",
   },
   twitter: {
     card: "summary_large_image",
     site: "@mechaneyes",
-    title: "Ray Weitzenberg",
-    description: "Try to find me.",
-    image: "https://nysee.nyc/images/twitter-card.png",
+    title: "Present Ex Machina",
+    description: "Presenting Scopus Ex Machina",
+    image: "https://nysee.nyc/images/og-presentation.png",
   },
   robots: "index, follow",
   keywords:
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body className={inter.className}>
         {children}
